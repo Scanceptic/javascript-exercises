f03515406a9b7ef1944ef3c63ab81af4d7f6c437
Nathan <urwin.nathan@gmail.com>
2024-06-21 16:29:48 +0100

    Complete js-ex-08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,57 @@
-const add = function() {
-	
+const add = function(...nums) {
+  let result = nums[0] + nums[1];
+  
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(...nums) {
+	let result = nums[0] - nums[1];
+
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let total = 0;
+
+  for (let i=0; i < nums.length; i++) {
+    total += nums[i];
+  }
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  let total = nums[0];
 
+  for (let i=1; i < nums.length; i++) {
+    total *= nums[i];
+  }
+
+  return total;
 };
 
-const power = function() {
-	
+const power = function(...nums) {
+	let total = nums[0];
+  
+  for (let i=1; i < nums[1]; i++) {
+    total *= nums[0];
+  }
+
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+
+	let total = num;
+  
+  for (let i=num-1; i>0; i--) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
