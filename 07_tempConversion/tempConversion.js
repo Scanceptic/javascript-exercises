43e6cc6a0e8bc3d8d1cdef76eca6301c94497688
Nathan <urwin.nathan@gmail.com>
2024-06-21 16:15:18 +0100

    Complete js-ex-07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  celsius = (temp - 32) * (5/9);
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  farenheit = (temp * (9/5)) + 32;
+  farenheit = Math.round(farenheit * 10) / 10;
+  return farenheit;
 };
 
 // Do not edit below this line
