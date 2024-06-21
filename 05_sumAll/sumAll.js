cf3b9e695ad8f0d75a31b34211024841d40df76d
Nathan <urwin.nathan@gmail.com>
2024-06-21 15:55:50 +0100

    Complete js-ex-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(...args) {    
+    // Confirm 2 parameters only
+    if (args.length !== 2) {
+        return 'ERROR';
+    }
+    // Confirm number parameters
+    if (typeof(args[0]) !== 'number' || typeof(args[1]) !== 'number') {
+        return 'ERROR';
+    }
+    // Confirm positive number parameters
+    if (args[0] < 1 || args[1] < 1) {
+        return 'ERROR';
+    }
 
+    let sortedArgs = args.sort();
+    let sum = 0;
+    
+    for (let i = sortedArgs[0]; i <= sortedArgs[1]; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
