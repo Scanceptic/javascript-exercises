aff71c25eb37487ff14f3b3f81d9a86316fa350e
Nathan <urwin.nathan@gmail.com>
2024-06-24 08:07:16 +0100

    Complete js-ex-10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
 
+    const fibSeq = [1, 1];
+
+    if (num < 0) {
+        return "OOPS";
+    } else if (num === 0) {
+        return 0;
+    } else {
+        for (let i = 3; i <= num; i++) {
+            let nextNum = fibSeq[i-3] + fibSeq[i-2];
+            fibSeq.push(nextNum);
+        }
+    }
+    return fibSeq[num-1];
 };
 
 // Do not edit below this line
